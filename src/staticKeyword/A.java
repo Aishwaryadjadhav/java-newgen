1e96df2cdc39a0f515b59da01702d1de409291a1
Aishwaryadjadhav <aishwaryadj1@gmail.com>
2024-12-31 21:48:53 +0530

    initial commit

---
+++ src/staticKeyword/A.java
@@ -0,0 +1,19 @@
+package staticKeyword;
+
+public class A {
+	
+	static int b;
+
+    static {
+        System.out.println("welcome to static block");
+        b = 12;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to main method");
+        System.out.println(A.b);
+        A.b = 13;
+        System.out.println(A.b);
+    }
+
+}
---
+++ src/staticKeyword/B_Test.java
@@ -0,0 +1,10 @@
+package staticKeyword;
+
+public class B_Test {
+	
+	 public static void main(String[] args) {
+	        B b = new B(12, "Akash");
+	        b.print();
+	    }
+
+}
