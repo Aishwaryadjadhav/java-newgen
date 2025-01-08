e84002c5df234066ea228c2f105ac521670003ac
Aishwaryadjadhav <aishwaryadj1@gmail.com>
2025-01-08 21:30:23 +0530

    first commit

---
+++ src/exceptions/InvalidNamesException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class InvalidNamesException extends Exception {
+	
+	public InvalidNamesException() {
+    }
+
+    public InvalidNamesException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/exceptions/NegativeNumberException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class NegativeNumberException extends RuntimeException {
+	
+	 public NegativeNumberException() {
+	    }
+
+	    public NegativeNumberException(String message) {
+	        super(message);
+	    }
+
+}
