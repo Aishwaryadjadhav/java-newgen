bf2172bee3cc37cdd407781df00980b54bb07b00
Aishwaryadjadhav <aishwaryadj1@gmail.com>
2025-01-09 18:55:56 +0530

    first commit

---
+++ src/multithreading/ThreadWithRunnable.java
@@ -0,0 +1,19 @@
+package multithreading;
+
+public class ThreadWithRunnable implements Runnable {
+	
+	@Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " thread started");
+        for (int i = 0; i < 5; i++) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + " is running");
+        }
+
+    }
+
+}
