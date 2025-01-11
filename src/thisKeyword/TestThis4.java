9245106a599ce9205c252b5db4589d3d6bbf42ca
Aishwaryadjadhav <aishwaryadj1@gmail.com>
2025-01-11 16:30:33 +0530

    first commit

---
+++ src/thisKeyword/TestThis4.java
@@ -0,0 +1,20 @@
+package thisKeyword;
+
+public class TestThis4 {
+	
+	public static void main(String args[]) {
+        A a = new A();
+        a.n();
+    }
+	
+}
+
+//4 Saurabh
+//5
+//hello a
+
+//hello n
+//Hello you are in the Print Msg
+//hello m
+
+
