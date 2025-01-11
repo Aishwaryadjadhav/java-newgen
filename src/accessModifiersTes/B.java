4e9fc8d2d9181bece9252fffa5c2d995744764cf
Aishwaryadjadhav <aishwaryadj1@gmail.com>
2025-01-11 16:29:27 +0530

    initial commit

---
+++ src/accessModifiersTes/B.java
@@ -0,0 +1,13 @@
+package accessModifiersTes;
+
+import accessModifiers.A;
+
+public class B extends A {
+	
+
+	 public static void main(String[] args) {
+	        B b = new B();
+	        System.out.println(b.x);
+	    }
+
+}
---
+++ src/accessModifiersTes/Test.java
@@ -0,0 +1,11 @@
+package accessModifiersTes;
+import accessModifiers.A;
+
+public class Test {
+	
+	public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.x);
+    }
+
+}
